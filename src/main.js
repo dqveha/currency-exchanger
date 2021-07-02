6f1ee117d153c3c4c33119879faf2610dbcc5252
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-02 14:26:04 -0700

    Update displayConversionRate() branching with two different types of error messages

--- src/main.js
+++ src/main.js
@@ -5,17 +5,14 @@ import "./css/styles.css";
 import CurrencyExchanger from "./services/currency-exchange.js";
 
 function formatNumber(num) {
-  return num.toString().replace(/\d{1,3}(?=(\d{3})+(?=\.))/g, "$1,");
+  return num.toString().replace(/(\d)(?=(\d{3})+(?=\.))/g, '$1,')
 }
 
 function displayConversionRate(response) {
-  if (
-    `${response["error-type"]}` === "unsupported-code" ||
-    `${response["error-type"]}` === "malformed-request"
-  ) {
-    $("#show-conversion").html(
-      "Please try again with a different code that's supported."
-    );
+  console.log(response);
+  if (response["error-type"] == "unsupported-code" || response["error-type"] == "malformed-request" || (response["error-type"] === undefined && response.result !== "success")) {
+    $("#show-conversion")
+    .html(`<table>ERROR: ${response["error-type"]}.<hr>Please fix your values with the appropriate currency code as indicated on the most left-hand column of the table.</table>`).show();
   } else if (response.conversion_rate) {
     const conversionResult = formatNumber(response.conversion_result.toFixed(4));
     const conversionRate = formatNumber(response.conversion_rate.toFixed(4));
@@ -35,12 +32,12 @@ function displayConversionRate(response) {
       .show();
   } else {
     $("#show-conversion")
-      .text(`ERROR: ${response["error-type"]}. Please fix your values with the correct currency code as indicated on the table, left-hand column.`).show();
+    .html(`<table>ERROR: ${response["error-type"]}.<hr>For more information, please visit the "Error Responses" section within this <a href="https://www.exchangerate-api.com/docs/pair-conversion-requests">link</a> here.`).show();
   }
 }
 
 $(document).ready(function () {
-  $("#currency-form").submit(function (event) {
+  $("input#submit").click(function (event) {
     event.preventDefault();
     $("input").empty();
     let amount = $("input#amount-currency1").val();
--- src/services/currency-exchange.js
+++ src/services/currency-exchange.js
@@ -1,17 +1,15 @@
 export default class CurrencyExchanger {
+
   static getExchangePair(amount, currency1, currency2) {
-    return fetch(
-      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`
-    )
-      .then(function (response) {
-        if (!response.ok) {
-          console.log(response.json());
-          return response.json();
-        }
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`)
+    .then(function(response) {
+      if (!response.ok) {
         return response.json();
-      })
-      .catch(function (error) {
-        return Error(error);
-      });
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return Error(error);
+    })
   }
 }
