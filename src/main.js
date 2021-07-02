5230b4165901dd59e70f254f639079b05dc49bd5
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-02 13:27:32 -0700

    Update conversion result and rate output with commas

--- src/main.js
+++ src/main.js
@@ -1,51 +1,67 @@
-import $ from 'jquery'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './css/styles.css'
-import CurrencyExchanger from './services/currency-exchange.js'
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import CurrencyExchanger from "./services/currency-exchange.js";
 
+function formatNumber(num) {
+  return num.toString().replace(/\d{1,3}(?=(\d{3})+(?=\.))/g, "$1,");
+}
 
 function displayConversionRate(response) {
-  if (response.conversion_rate) {
-    $('#show-conversion').html(`
-      <table>
+  if (
+    `${response["error-type"]}` === "unsupported-code" ||
+    `${response["error-type"]}` === "malformed-request"
+  ) {
+    $("#show-conversion").html(
+      "Please try again with a different code that's supported."
+    );
+  } else if (response.conversion_rate) {
+    const conversionResult = formatNumber(response.conversion_result.toFixed(4));
+    const conversionRate = formatNumber(response.conversion_rate.toFixed(4));
+    $("#show-conversion")
+      .html(
+        `<table>
         <tr>
           <th>Total Amount Converted</th>
           <th>Conversion Rate</th>
         </tr>
         <tr>
-          <td>${response.conversion_result.toFixed(3)}</td>
-          <td>${response.conversion_rate.toFixed(3)}</td>
+          <td>${conversionResult}</td>
+          <td>${conversionRate}</td>
         </tr>
-      </table>`).show();
+      </table>`
+      )
+      .show();
   } else {
-    $('#show-conversion').text(`There was an error: ${response["error-type"]}`).show();
+    $("#show-conversion")
+      .text(`ERROR: ${response["error-type"]}. Please fix your values with the correct currency code as indicated on the table, left-hand column.`).show();
   }
 }
 
-
-$(document).ready(function() {
-  $("#currency-form").submit(function(event) {
+$(document).ready(function () {
+  $("#currency-form").submit(function (event) {
     event.preventDefault();
     $("input").empty();
-    let amount = $('input#amount-currency1').val();
-    let currency1 = $('input#currency1').val();
-    let currency2 = $('input#currency2').val();
-    CurrencyExchanger.getExchangePair(amount, currency1, currency2)
-    .then(function(response) {
-      displayConversionRate(response);
-    });
-  })
+    let amount = $("input#amount-currency1").val();
+    let currency1 = $("input#currency1").val();
+    let currency2 = $("input#currency2").val();
+    CurrencyExchanger.getExchangePair(amount, currency1, currency2).then(
+      function (response) {
+        displayConversionRate(response);
+      }
+    );
+  });
 
   $("input#reset").click(function () {
     $("input").empty();
     $("#show-conversion").hide();
   });
 
-  $("input#switch").click(function() {
-    let currency1 = $('input#currency1').val();
-    let currency2 = $('input#currency2').val();
-    $('input#currency1').val(currency2);
-    $('input#currency2').val(currency1);
-  })
+  $("input#switch").click(function () {
+    let currency1 = $("input#currency1").val();
+    let currency2 = $("input#currency2").val();
+    $("input#currency1").val(currency2);
+    $("input#currency2").val(currency1);
+  });
 });
--- src/services/currency-exchange.js
+++ src/services/currency-exchange.js
@@ -1,15 +1,17 @@
 export default class CurrencyExchanger {
-
   static getExchangePair(amount, currency1, currency2) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`)
-    .then(function(response) {
-      if (!response.ok) {
+    return fetch(
+      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`
+    )
+      .then(function (response) {
+        if (!response.ok) {
+          console.log(response.json());
+          return response.json();
+        }
         return response.json();
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return Error(error);
-    })
+      })
+      .catch(function (error) {
+        return Error(error);
+      });
   }
 }
