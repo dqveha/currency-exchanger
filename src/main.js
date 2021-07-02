ff4dc78d5ad0ff918c99d390c62200103c51134b
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-02 11:02:26 -0700

    WIP: Submitting currency code to output conversion rate

--- src/main.js
+++ src/main.js
@@ -7,21 +7,28 @@ import CurrencyExchanger from './services/currency-exchange.js'
 
 function displayConversionRate(response) {
   if (response.conversion_rate) {
-    $('#show-conversion').text(`${response.conversion_rate}`);
+    $('#show-conversion').text(`${response.conversion_rate}`).show();
   } else {
-    $('#show-conversion').text(`There was an error: ${response["error-type"]}`)
+    $('#show-conversion').text(`There was an error: ${response["error-type"]}`).show();
   }
 }
 
-console.log(CurrencyExchanger);
 
 $(document).ready(function() {
-  let currency1 = $('#currency1').val();
-  let currency2 = $('#currency2').val();
+  let currency1 = $('input#currency1').val();
+  let currency2 = $('input#currency2').val();
 
-  CurrencyExchanger.getExchangePair(currency1, currency2)
-  .then(function(response) {
-    displayConversionRate(response);
+  $("#currency-form").submit(function(event) {
+    event.preventDefault();
+    CurrencyExchanger.getExchangePair("USD", "USD")
+    .then(function(response) {
+      displayConversionRate(response);
+    });
+    console.log(currency1);
   })
+
+  $("input#reset").click(function () {
+    $("input").empty();
+  });
 
 });
--- src/services/currency-exchange.js
+++ src/services/currency-exchange.js
@@ -1,9 +1,4 @@
 export default class CurrencyExchanger {
-  constructor() {
-    this.currency1 = "";
-    this.currency2 = "";
-    this.conversion = "";
-  }
 
   static getExchangePair(currency1, currency2) {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}`)
